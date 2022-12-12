f6ed496697196af3589309314c724547c1c46ca5
Ivan Hamou <ivanhamou@gmail.com>
2022-12-12 09:50:12 +0100

    added firebase functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getFirestore, addDoc, collection } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
+import { getFirestore, addDoc, collection, getDocs, doc, deleteDoc } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -26,7 +26,11 @@ const mainPage = document.querySelector(".mainPage")
 const addtoWatch = document.querySelector(".addtoWatch")
 const goBack = document.querySelector(".goBack")
 const watchlist = document.querySelector(".watchlist")
+const movieContainer = document.querySelector(".movieContainer")
 const openWatch = document.querySelector(".openWatch")
+const removeMovie = document.querySelector(".removeMovie")
+let movieID 
+let movieName
 console.log(movieTitle[0].innerText);
 
 
@@ -36,7 +40,6 @@ watchlist.style.display = "none"
 
 for (let i = 0; i < movieTitle.length; i++) {   
     movieTitle[i].addEventListener("click", () => {
-        movieOptions.classList.add("show")
         movieOptions.style.removeProperty("display")
         mainPage.style.display = "none"
             
@@ -53,51 +56,76 @@ for (let i = 0; i < movieTitle.length; i++) {
             <p>2020-03-23</p>
         </article>`
     
-        //movieOptions.innerText= ""
-        movieOptions.insertAdjacentHTML("beforeend", elem)
-
-
-        goBack.addEventListener("click", () => {
-            mainPage.style.display = "block"
-            movieOptions.style.display = "none"
-            watchlist.style.display = "none"
-            console.log(elem);
-            elem.replace("article", "ss")
-            console.log(elem);
-        })
+        movieContainer.insertAdjacentHTML("beforeend", elem)
         
-        addtoWatch.addEventListener("click", () => {
-            const elem = `
-            <article class="movieTitle">
-            <h2>${movieTitle[i].innerText}</h2>
-            </article>
-            <article class="movieGenre">
-            <h4>Genre </h4>
-            <p>Horror</p>
-            </article>
-            <article class="movieDate">
-            <h4>Release-date </h4>
-            <p>2020-03-23</p>
-            </article>`
-            
-            watchlist.insertAdjacentHTML("beforeend", elem)
-            
-            //saveToDatabase()
-            tt = movieTitle[i].innerText
-            console.log(tt);
-            saveToDatabase()
-        })
+        movieName = movieTitle[i].innerText
+        addtoWatch.addEventListener("click", addtowatch)
     })
 };
 
- let tt 
+
+goBack.addEventListener("click", () => {
+    mainPage.style.display = "block"
+    movieOptions.style.display = "none"
+    watchlist.style.display = "none"
+    movieContainer.innerHTML = ""
+    console.log(movieName);
+
+})
+
+function addtowatch() {
+    const elem = `
+    <button class="removeMovie">Remove from watchlist</button>
+    <article class="movieTitle">
+    <h2>${movieName}</h2>
+    </article>
+    <article class="movieGenre">
+    <h4>Genre </h4>
+    <p>Horror</p>
+    </article>
+    <article class="movieDate">
+    <h4>Release-date </h4>
+    <p>2020-03-23</p>
+    </article>`
+    
+    watchlist.insertAdjacentHTML("beforeend", elem)
+    const removeMovie = document.querySelector(".removeMovie")
+    console.log(removeMovie);
+    alert("Movie has been added to watchlist")
+    movieName
+    saveToDatabase()
+    removeEvent()
+
+    removeMovie.addEventListener("click", () => {
+        removeFromDatabase()
+        getAllMovies()
+        watchlist.innerHTML = ""
+    })
+}
+
+function removeEvent() {
+        addtoWatch.removeEventListener("click", addtowatch)
+}
 
 
 async function saveToDatabase() {
     await addDoc(collection(db, 'Watchlist'), {  
-        Movie:{tt}
-
+        movieName
     });     
+}
+
+async function getAllMovies() {
+    const selected = await getDocs(collection(db, 'Watchlist'));  
+     
+    selected.forEach(movie => {
+        movieID = movie.id 
+    });    
+}
+
+async function removeFromDatabase() {     
+    
+    await deleteDoc(doc(db, "Watchlist", movieID))// where("selectedPlanet", "==", ""))
+    console.log(movieID);
 }
 
 
@@ -109,3 +137,5 @@ openWatch.addEventListener("click", () => {
         watchlist.style.removeProperty("display")
         mainPage.style.display = "none"  
 })
+
+
